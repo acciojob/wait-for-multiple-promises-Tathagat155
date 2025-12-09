853d85496834f19473ba0dcc6243f7e06f956e36
Tathagat155 <tathagatkumar70618@gmail.com>
2025-12-09 13:54:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+  const tbody=document.getElementById("output");
+  function createRandomPromise(){
+       let time=Math.random()*2+1;
+	   return new Promise((resolve,reject)=>{
+          setTimeout(()=>{
+              resolve(time);
+		  },time*1000) // to become millisecond
+	   })
+  }
+  const p1=createRandomPromise();
+  const p2=createRandomPromise();
+  const p3=createRandomPromise();
+   let startTime=performance.now(); 
+
+  Promise.all([p1,p2,p3]).then((data)=>{ 
+       let endTime=performance.now();
+	   let totalTime=endTime-startTime;  //performance.now give time in milisecond
+	     //convert totaltime in seconds
+	   totalTime=totalTime/1000;
+	  tbody.innerHTML="";
+	  data.forEach((time,index)=>{
+		  tbody.innerHTML+=`<tr><td>Promise ${index+1}</td><td>${time.toFixed(3)}</td></tr>`
+	  })
+	  tbody.innerHTML+=`<tr><td>Total</td><td>${totalTime.toFixed()}</td></tr>`
+  })
